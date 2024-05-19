9a03d7459f108b75ea7d0cc4789cd551800c151c
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2024-05-19 15:46:41 +0530

    database setup

---
+++ src/databse.ts
@@ -0,0 +1,24 @@
+import { winstonLogger } from '@slasher190/jobber-app';
+import { Logger } from 'winston';
+import { config } from '@auth/config';
+import { Sequelize } from 'sequelize';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SERACH_URL}`, 'authDatabaseServer', 'debug');
+
+export const sequelze: Sequelize = new Sequelize({
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    multipleStatements: true
+  }
+});
+
+export async function databaseConnection(): Promise<void> {
+  try {
+    await sequelze.authenticate();
+    log.info('AuthService MySql database connection has been establish successfully.');
+  } catch (error) {
+    log.error('Auth Service - Unable to connect to database.');
+    log.log('error', 'Authservice databaseConnection() method error:', error);
+  }
+}
